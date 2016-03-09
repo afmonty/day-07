3b5c13e724062e5027cb28eea23b5fd88f1a5c8f
anna hunter <annfmonty@gmail.com>
2016-03-09 08:42:32 -0600

    its finally right-ish

--- assignments/cmd-ln-quiz/scripts/main.js
+++ assignments/cmd-ln-quiz/scripts/main.js
@@ -7,16 +7,18 @@ var rtAnswer = 0;
 
 //Question 1
 answer = prompt("Which is a datatype in Javascript? \n A = var, B = string, or C = <= ");
-if (answer == "b" || "B"){
+answer .toLowerCase(answer);
+if (answer == "b"){
 	rtAnswer++;
 	console.log("You got "+rtAnswer+" correct.  Next Question");
 }
 	else{
 		console.log("Next Question")
-	};
+	}
 //Question 2
 answer = prompt("How do you close a <Div>"+"\nA = </Div>, B = >, or C = ;");
-if(answer == "a" || "A"){
+answer .toLowerCase(answer);
+if(answer == "a" ){
 	rtAnswer = rtAnswer + 1;
 	console.log("You got "+rtAnswer+" correct.  Next Question");
 }
@@ -25,7 +27,8 @@ if(answer == "a" || "A"){
 	};
 //Question 3
 answer = prompt("Which is an example of a HTML tag that does not require a closing tag"+"\nA = div, B = img, or C = script");
-if(answer == "b" || "B"){
+answer .toLowerCase(answer);
+if(answer == "b" ){
 	rtAnswer = rtAnswer + 1;
 	console.log("You got "+rtAnswer+" correct.  Next Question");
 }
@@ -34,7 +37,8 @@ if(answer == "b" || "B"){
 	};
 	//Question 4
 answer = prompt("In the CSS box model, Border and Padding are two elements around the content.  What is the third"+"\nA = Margin, B = Outside, or C = border box");
-if(answer == "a" || "A"){
+answer .toLowerCase(answer);
+if(answer == "a"){
 	rtAnswer = rtAnswer + 1;
 	console.log("You got "+rtAnswer+" correct.  Next Question");
 }
@@ -42,8 +46,10 @@ if(answer == "a" || "A"){
 		console.log("Next Question")
 	};
 //Question 5
+ 
 answer = prompt("What does '1' + 5 + 5 evaluate to?"+"\nA = 11, B = 25, or C = 155");
-if(answer == "c" || "C"){
+answer .toLowerCase(answer);
+if(answer == "c"){
 	rtAnswer = rtAnswer + 1;
 	console.log("You got "+rtAnswer+" correct.  Next Question");
 }
@@ -51,8 +57,10 @@ if(answer == "c" || "C"){
 		console.log("Next Question")
 	};
 //Question 6
+
 answer = prompt("How do you call a .css file in HTLM"+"\nA = <call>, B = <script>, or C = <link>");
-if(answer == "c" || "C"){
+answer .toLowerCase(answer);
+if(answer == "c"){
 	rtAnswer = rtAnswer + 1;
 	console.log("You got "+rtAnswer+" correct.  Next Question");
 }
@@ -60,8 +68,10 @@ if(answer == "c" || "C"){
 		console.log("Next Question")
 	};
 	//Question 7
+
 answer = prompt("In DHTML, The D is for dynamic"+"\nA = True or B = False");
-if(answer == "a" || "A"){
+answer .toLowerCase(answer);
+if(answer == "a"){
 	rtAnswer = rtAnswer + 1;
 	console.log("You got "+rtAnswer+" correct.  Next Question");
 }
@@ -69,8 +79,10 @@ if(answer == "a" || "A"){
 		console.log("Next Question")
 	};
 	//Question 8
+	 
 answer = prompt("In Javascript, '>=' is a unary operator"+"\nA = True or B = False");
-if(answer == "b" || "B"){
+answer .toLowerCase(answer);
+if(answer == "b"){
 	rtAnswer = rtAnswer + 1;
 	console.log("You got "+rtAnswer+" correct.  Next Question");
 }
@@ -78,8 +90,10 @@ if(answer == "b" || "B"){
 		console.log("Next Question")
 	};
 	//Question 9
+	 
 answer = prompt("What is another name for Javascript"+"\nA = Java, B = Coffee talk, or C = ECMAScript");
-if(answer == "c" || "C"){
+answer .toLowerCase(answer);
+if(answer == "c" ){
 	rtAnswer = rtAnswer + 1;
 	console.log("You got "+rtAnswer+" correct.  Next Question");
 }
@@ -88,26 +102,32 @@ if(answer == "c" || "C"){
 	};
 
 	//Question 10
+	 
 answer = prompt("In Javascript, what is NaN"+"\nA = your Grandmother, B = flat bread, or C = Not a Number" );
-if(answer == "c" || "C"){
+answer .toLowerCase(answer);
+if(answer == "c"){
 	rtAnswer = rtAnswer + 1;
 	console.log("You got "+rtAnswer+" correct.  Next Question");
 }
 	else{
 		console.log("Next Question")
 	};
-		//Question 11
+	//Question 11
+	 
 answer = prompt("What does 3 + 5 + '4' evaluate to?"+"\nA = 23, B = 84, or C = 12");
-if(answer == "b" || "B"){
+answer .toLowerCase(answer);
+if(answer == "b"){
 	rtAnswer = rtAnswer + 1;
 	console.log("You got "+rtAnswer+" correct.  Next Question");
 }
 	else{
 		console.log("Next Question")
 	};
-		//Question 12
+	//Question 12
+	 
 answer = prompt("What will Math.ceil(9.0115) return?"+"\nA = 10, B = 9, or C = Not a Number" );
-if(answer == "a" || "A"){
+answer .toLowerCase(answer);
+if(answer == "a"){
 	rtAnswer = rtAnswer + 1;
 	console.log("You got "+rtAnswer+" correct.  Next Question");
 }
@@ -115,8 +135,10 @@ if(answer == "a" || "A"){
 		console.log("Next Question")
 	};
 		//Question 13
+		 
 answer = prompt("What is the terminal command to create a new directory"+"\nA = abraca-directory, B = crdir, or C = mkdir" );
-if(answer == "c" || "C"){
+answer .toLowerCase(answer);
+if(answer == "c"){
 	rtAnswer = rtAnswer + 1;
 	console.log("You got "+rtAnswer+" correct.  Next Question");
 }
@@ -124,20 +146,24 @@ if(answer == "c" || "C"){
 		console.log("Next Question")
 	};
 		//Question 14
+		 
 answer = prompt("The 3 main ways to apply CSS style to a web page are In-line, Embedded/Internal and Linked/External?"+"\nA = Ture, B = False" );
-if(answer == "a" || "A"){
+answer .toLowerCase(answer);
+if(answer == "a"){
 	rtAnswer = rtAnswer + 1;
 	console.log("You got "+rtAnswer+" correct.  Last Question");
 }
 	else{
 		console.log("Last Question")
 	};
-			//Question 15
+	//Question 15
+		 
 answer = prompt("Is Javascript case sensative"+"\nA = yes, B = NO, or C = maYbE" );
-if(answer == "a" || "A"){
+answer .toLowerCase(answer);
+if(answer == "a"){
 	rtAnswer = rtAnswer + 1;
-	prompt("You got "+rtAnswer+" correct.  Thanks for playing");
+	alert("You got "+rtAnswer+" correct.  Thanks for playing");
 }
 	else{
-		prompt("You got "+rtAnswer+" correct.  Thanks for playing")
+		alert("You got "+rtAnswer+" correct.  Thanks for playing")
 	};
