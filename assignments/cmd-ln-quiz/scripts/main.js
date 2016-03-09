269005918fc9d5b477c8df122611b70333fa0979
anna hunter <annfmonty@gmail.com>
2016-03-08 19:02:22 -0600

    questions 1 - 10

--- assignments/cmd-ln-quiz/scripts/main.js
+++ assignments/cmd-ln-quiz/scripts/main.js
@@ -5,22 +5,130 @@ alert("This is a test of what we have learned so far!");
 var answer;
 var rtAnswer = 0;
 
+//Question 1
 answer = prompt("Which is a datatype in Javascript? \n A = var, B = string, or C = <= ");
-
-if (answer == "B"){
+if (answer == "b" || "B"){
 	rtAnswer++;
 	document.write("You got "+rtAnswer+" correct.  Next Question");
 }
 	else{
 		document.write("Next Question")
 	};
-
+//Question 2
 answer = prompt("How do you close a <Div>"+"\nA = </Div>, B = >, or C = ;");
-
-if(answer == A){
+if(answer == "a" || "A"){
+	rtAnswer = rtAnswer + 1;
 	document.write("You got "+rtAnswer+" correct.  Next Question");
-	rtAnswer++;
 }
 	else{
 		document.write("Next Question")
-	}
+	};
+//Question 3
+answer = prompt("Which is an example of a HTML tag that does not require a closing tag"+"\nA = div, B = img, or C = script");
+if(answer == "b" || "B"){
+	rtAnswer = rtAnswer + 1;
+	document.write("You got "+rtAnswer+" correct.  Next Question");
+}
+	else{
+		document.write("Next Question")
+	};
+	//Question 4
+answer = prompt("In the CSS box model, Border and Padding are two elements.  What is the third"+"\nA = Margin, B = Outside, or C = border box");
+if(answer == "a" || "A"){
+	rtAnswer = rtAnswer + 1;
+	document.write("You got "+rtAnswer+" correct.  Next Question");
+}
+	else{
+		document.write("Next Question")
+	};
+//Question 5
+answer = prompt("What does '1' + 1 + 1 evaluate to?"+"\nA = 11, B = 25, or C = 155");
+if(answer == "c" || "C"){
+	rtAnswer = rtAnswer + 1;
+	document.write("You got "+rtAnswer+" correct.  Next Question");
+}
+	else{
+		document.write("Next Question")
+	};
+//Question 6
+answer = prompt("How do you call a .css file in HTLM"+"\nA = <call>, B = <script>, or C = <link>");
+if(answer == "c" || "C"){
+	rtAnswer = rtAnswer + 1;
+	document.write("You got "+rtAnswer+" correct.  Next Question");
+}
+	else{
+		document.write("Next Question")
+	};
+	//Question 7
+answer = prompt("In DHTML, The D is for dynamic"+"\nA = True or B = False");
+if(answer == "a" || "A"){
+	rtAnswer = rtAnswer + 1;
+	document.write("You got "+rtAnswer+" correct.  Next Question");
+}
+	else{
+		document.write("Next Question")
+	};
+	//Question 8
+answer = prompt("In Javascript, '>=' is a unary operator"+"\nA = True or B = False");
+if(answer == "b" || "B"){
+	rtAnswer = rtAnswer + 1;
+	document.write("You got "+rtAnswer+" correct.  Next Question");
+}
+	else{
+		document.write("Next Question")
+	};
+	//Question 9
+answer = prompt("What is another name for Javascript"+"\nA = Java, B = Coffee talk, or C = ECMAScript");
+if(answer == "c" || "C"){
+	rtAnswer = rtAnswer + 1;
+	document.write("You got "+rtAnswer+" correct.  Next Question");
+}
+	else{
+		document.write("Next Question")
+	};
+
+	//Question 10
+answer = prompt("In Javascript, what is NaN"+"\nA = your Grandmother, B = flat bread, or C = Not a Number" );
+if(answer == "c" || "C"){
+	rtAnswer = rtAnswer + 1;
+	document.write("You got "+rtAnswer+" correct.  Next Question");
+}
+	else{
+		document.write("Next Question")
+	};
+		//Question 11
+answer = prompt("What does 2+5+"8" evaluate to?"+"\nA = 23, B = 84, or C = 12" );
+if(answer == "b" || "B"){
+	rtAnswer = rtAnswer + 1;
+	document.write("You got "+rtAnswer+" correct.  Next Question");
+}
+	else{
+		document.write("Next Question")
+	};
+		//Question 13
+answer = prompt("Is Javascript, what is NaN"+"\nA = your Grandmother, B = flat bread, or C = Not a Number" );
+if(answer == "c" || "C"){
+	rtAnswer = rtAnswer + 1;
+	document.write("You got "+rtAnswer+" correct.  Next Question");
+}
+	else{
+		document.write("Next Question")
+	};
+		//Question 14
+answer = prompt("Is Javascript, what is NaN"+"\nA = your Grandmother, B = flat bread, or C = Not a Number" );
+if(answer == "c" || "C"){
+	rtAnswer = rtAnswer + 1;
+	document.write("You got "+rtAnswer+" correct.  Next Question");
+}
+	else{
+		document.write("Next Question")
+	};
+		//Question 15
+answer = prompt("Is Javascript, what is NaN"+"\nA = your Grandmother, B = flat bread, or C = Not a Number" );
+if(answer == "c" || "C"){
+	rtAnswer = rtAnswer + 1;
+	document.write("You got "+rtAnswer+" correct.  Next Question");
+}
+	else{
+		document.write("Next Question")
+	};
